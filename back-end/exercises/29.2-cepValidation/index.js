c50ccd13451cc76c9729b9e6e2e09ebbd928452b
moraesvictor <moraesvictor@gmail.com>
2021-10-27 10:50:18 -0300

    feat(trybe29): add eslint-config

--- back-end/exercises/29.2-cepValidation/index.js
+++ back-end/exercises/29.2-cepValidation/index.js
@@ -1,51 +1,47 @@
-const { default: axios } = require("axios");
+const { default: axios } = require('axios');
 
 const USERS_CEP = {
   iveteMoraes: {
-    cep: "03.564-020"
+    cep: '03.564-020',
   },
 
   victorMoraes: {
-    cep: "03.590-010"
-  }
-}
+    cep: '03.590-010',
+  },
+};
 
 const validatingCep = (CEP) => {
   const REGEX_CEP = /^[0-9]{2}.[0-9]{3}-[0-9]{3}$/;
   const validation = REGEX_CEP.test(CEP);
 
   return validation;
-}
+};
 
-const brasilApiService = async(cep) => {
+const brasilApiService = async (cep) => {
   const URL = `https://brasilapi.com.br/api/cep/v1/${cep}`;
 
   const request = await axios.get(URL);
 
-  console.log(request.data)
-}
+  console.log(request.data);
+};
 
-const viaCepService = async(cep) => {
+const viaCepService = async (cep) => {
   const URL = `https://viacep.com.br/ws/${cep}/json`;
 
   const request = await axios.get(URL);
 
-  console.log(request.data)
-}
+  console.log(request.data);
+};
 
-const ConsultingCep = async(userName, service) => {
-    const { cep } = USERS_CEP[`${userName}`];
+const ConsultingCep = async (userName, service) => {
+  const { cep } = USERS_CEP[userName];
 
-    const validation = validatingCep(cep);
-  
-    let verifiedCep;
+  const validation = validatingCep(cep);
 
-    if(validation) {
-      verifiedCep = cep.replace(/[^\d]+/g, "");
-      service(cep);
-    }
+  if (validation) {
+    service(cep.replace(/[^\d]+/g, ''));
+  }
+};
 
-}
-
-ConsultingCep("victorMoraes", brasilApiService)
-ConsultingCep("iveteMoraes", brasilApiService)
+ConsultingCep('victorMoraes', brasilApiService);
+ConsultingCep('iveteMoraes', viaCepService);
